ca8b36b15c1c2d58e182581d27cefefeb13c24bd
Bill Swartz <bill-swartz@utdallas.edu>
2019-01-24 09:03:55 -0600

    Added sverilog_parser_set_preprocess_output and sverilog_parser_get_errors.

--- include/verilog/parse.h
+++ include/verilog/parse.h
@@ -26,6 +26,14 @@ extern void sverilog_parser_add_default_callbacks( SVERILOG_PARSEPTR parse_p ) ;
 // The top level parsing interface
 extern int sverilog_parse_file( SVERILOG_PARSEPTR parse_p, char *filename ) ;
 
+//! Ask for the output of the preprocessor.  Output is sent to the specified
+// open file descriptor.
+extern void sverilog_parser_set_preprocess_output( SVERILOG_PARSEPTR parse_p,
+                                                   FILE *pre_output_p ) ;
+
+//! Get the number of errors found.
+extern int sverilog_parser_get_errors( SVERILOG_PARSEPTR parse_p ) ;
+
 #ifdef LATER
 
 extern verilog_source_tree *verilog_parser_get_source_tree(void) ;
